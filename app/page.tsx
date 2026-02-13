eb30ddd870d11706f624c88e4e35451546c66ec4
Todd McIntosh <todd@resonancemedia.ca>
2026-02-13 16:40:46 -0500

    Changed marbin bottom under intro section

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      <SectionDivider />
+      {/* <SectionDivider /> */}
       {/* <About /> */}
       <Projects />
       <History />
--- components/intro.tsx
+++ components/intro.tsx
@@ -18,7 +18,7 @@ export default function Intro() {
     <section
       ref={ref}
       id="home"
-      className="mb-28 max-w-[70rem] text-center sm:mb-0 scroll-mt-[100rem] "
+      className="mb-44 max-w-[70rem] text-center sm:mb-24 scroll-mt-[100rem] "
     >
       <div className="flex items-center justify-center">
         <div className="relative">
